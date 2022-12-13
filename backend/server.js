efac72ea2685de330745f5f8cfa3d5ee68c68a0e
mertangy <ericmagesho@gmail.com>
2022-12-13 09:29:23 +0300

    Made changes to mongo URI,app working locally

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const upload = multer();
+// require('dotenv').config();
 
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://admin:password@mongo:27017/';
-let dbName = 'yolomy';
+//let mongodb_url = 'mongodb://admin:password@mongo:27017/';
+//let dbName = 'yolomy';
 
 // define a url to connect to the database
 //process.env.MONGODB_URI
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+//mongodb_url + dbName
+
+const MONGODB_URI =  "mongodb+srv://matangi:matangi@gallery.cj6v9ym.mongodb.net/yolo?retryWrites=true&w=majority"
+
+//const MONGODB_URI = process.env.MONGODB_URI
+
+console.log(MONGODB_URI)
 
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
@@ -21,11 +28,12 @@ let db = mongoose.connection;
 // Check Connection
 db.once('open', ()=>{
     console.log('Database connected successfully')
-    console.log(MONGODB_URI)
+    
 })
 
 // Check for DB Errors
 db.on('error', (error)=>{
+    
     console.log(error);
 })
 
@@ -49,5 +57,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
-    console.log(MONGODB_URI)
+    
 })
